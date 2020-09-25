e74f5684c08bb8cda7615a39ec7b2b4eddb98df6
michael <nguyen.hoang.michael@gmail.com>
2020-09-25 16:34:29 +0200

    Adding screens to App.js

--- App.js
+++ App.js
@@ -1,12 +1,21 @@
 import React from 'react';
 import { StyleSheet, View, Text } from 'react-native';
+import Forgot from './screens/Forgot';
+import Friends from './screens/Friends';
+import Login from './screens/Login';
+import Profile from './screens/Profile';
+import Register from './screens/Register';
 
 export default class App extends React.Component {
 
     render() {
         return (
             <View style={styles.container}>
-                <Text>Bonjour, comment ca va ?</Text>
+                <Forgot />
+                <Friends />
+                <Login />
+                <Profile />
+                <Register />
             </View>
           );
     }
