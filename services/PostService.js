ddab42def8d93d8e55f208abcc8b7ff106694a97
michael <nguyen.hoang.michael@gmail.com>
2020-09-28 15:54:50 +0200

    Implementing PostService

--- services/PostService.js
+++ services/PostService.js
@@ -1,3 +1,50 @@
 export default class PostService {
+    posts = [
+        {
+            content: 'Salut je bois un verre au starbucks',
+            user_id: 2,
+            likes: 0
+        },
+        {
+            content: 'Je me promene dans le parc',
+            user_id: 1,
+            likes: 0
+        },
+        {
+            content: 'Je suis heureux',
+            user_id: 2,
+            likes: 4
+        },
+        {
+            content: 'Super, nous avons un beau cadeaux',
+            user_id: 2,
+            likes: 0
+        }
+    ]
 
+    list() {
+        return this.posts;
+    }
+
+    get(post_id) {
+        return this.posts[post_id];
+    }
+
+    create(user_id, content) {
+        const nouveauPost = {
+            content: content,
+            user_id: user_id,
+            likes: 0
+        }
+        this.posts.push(nouveauPost);
+    }
+
+    /**
+     * 
+     * key = "content", "likes"
+     */
+    update(post_id, key, value) {
+        const post = this.get(post_id);
+        post[key] = value;
+    }
 }
