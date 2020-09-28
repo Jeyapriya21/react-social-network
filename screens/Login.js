02d910914ef5580049ad23730c7ee63446127d39
michael <nguyen.hoang.michael@gmail.com>
2020-09-28 11:07:18 +0200

    Adding username input in login

--- screens/Login.js
+++ screens/Login.js
@@ -1,12 +1,23 @@
 import React from 'react';
-import { StyleSheet, View, Text, Button } from 'react-native';
+import { StyleSheet, View, Text, Button, TextInput } from 'react-native';
 
 export default class Login extends React.Component {
+
+    state = {
+        username: ''
+    }
+
+    handleChangeUsername = (text) => {
+        this.setState({
+            username: text
+        })
+    }
 
     render() {
         return (
             <View style={styles.container}>
                 <Text>Login</Text>
+                <TextInput value={this.state.username} onChangeText={this.handleChangeUsername} />
                 <Button title="Se logguer" onPress={this.props.changeScreen('Profile')} />
                 <Button title="S'enregister" onPress={this.props.changeScreen('Register')} />
                 <Button title="Mot de passe oublié"  onPress={this.props.changeScreen('Forgot')} />
