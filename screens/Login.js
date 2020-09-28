8115a637f62a732978126e3b9bab82dbe0c1b248
michael <nguyen.hoang.michael@gmail.com>
2020-09-28 11:54:25 +0200

    Adding text input in login

--- screens/Login.js
+++ screens/Login.js
@@ -4,7 +4,8 @@ import { StyleSheet, View, Text, Button, TextInput } from 'react-native';
 export default class Login extends React.Component {
 
     state = {
-        username: ''
+        username: '',
+        password: ''
     }
 
     handleChangeUsername = (text) => {
@@ -13,11 +14,18 @@ export default class Login extends React.Component {
         })
     }
 
+    handleChangePassword = (text) => {
+        this.setState({
+            password: text
+        })
+    }
+
     render() {
         return (
             <View style={styles.container}>
                 <Text>Login</Text>
                 <TextInput value={this.state.username} onChangeText={this.handleChangeUsername} />
+                <TextInput secureTextEntry={true} value={this.state.password} onChangeText={this.handleChangePassword} />
                 <Button title="Se logguer" onPress={this.props.changeScreen('Profile')} />
                 <Button title="S'enregister" onPress={this.props.changeScreen('Register')} />
                 <Button title="Mot de passe oublié"  onPress={this.props.changeScreen('Forgot')} />
