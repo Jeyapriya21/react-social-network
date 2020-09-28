08d5e5dafae21065372bc91fb03ae4de27f1c388
michael <nguyen.hoang.michael@gmail.com>
2020-09-28 11:59:59 +0200

    Adding input to forgot screen

--- screens/Forgot.js
+++ screens/Forgot.js
@@ -1,12 +1,23 @@
 import React from 'react';
-import { StyleSheet, View, Text, Button } from 'react-native';
+import { StyleSheet, View, Text, Button, TextInput } from 'react-native';
 
 export default class Forgot extends React.Component {
+
+    state = {
+        email: ''
+    }
+
+    handleChangeEmail = (text) => {
+        this.setState({
+            email: text
+        })
+    }
 
     render() {
         return (
             <View style={styles.container}>
                 <Text>Forgot</Text>
+                <TextInput value={this.state.email} onChangeText={this.handleChangeEmail} />
                 <Button title="Retour"  onPress={this.props.changeScreen('Login')} />
                 <Button title="Valider"  onPress={this.props.changeScreen('Login')} />
             </View>
