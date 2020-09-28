a29a9794f376834f807c1a4eb93db0caaf9ac80a
michael <nguyen.hoang.michael@gmail.com>
2020-09-28 15:17:56 +0200

    Implementing UserService

--- screens/Friends.js
+++ screens/Friends.js
@@ -1,12 +1,23 @@
 import React from 'react';
-import { StyleSheet, View, Text, Button } from 'react-native';
+import { StyleSheet, View, Text, Button, TextInput } from 'react-native';
 
 export default class Friends extends React.Component {
+
+    state = {
+        searchText: ''
+    }
+
+    handleChangeSearchText = (text) => {
+        this.setState({
+            searchText: text
+        })
+    }
 
     render() {
         return (
             <View style={styles.container}>
                 <Text>Friends</Text>
+                <TextInput value={this.state.searchText} onChangeText={this.handleChangeSearchText} />
                 <Button title="Retour"  onPress={this.props.changeScreen('Profile')} />
             </View>
           );
--- services/UserService.js
+++ services/UserService.js
@@ -1,3 +1,65 @@
 export default class UserService {
+    /**
+     * 
+     * CREATE
+     * RETRIEVE
+     * UPDATE
+     */
+    users = [
+        {
+            firstname: 'Sophie',
+            lastname: 'Tagada',
+            username: 'soso',
+            email: 'sophie@tag.com',
+            password: 'sophie'
+        },
+        {
+            firstname: 'Kevin',
+            lastname: 'Burt',
+            username: 'kevin.burt',
+            email: 'kevin@burt.com',
+            password: 'kevin'
+        },
+        {
+            firstname: 'Jeremy',
+            lastname: 'Cross',
+            username: 'jeremy.cross',
+            email: 'jeremy@cross.com',
+            password: 'jeremy'
+        }
+    ];
 
+    list() {
+        return this.users;
+    }
+
+    get(requestedUsername) {
+        const filteredUsers = this.users.filter(user => user.username == requestedUsername);
+        return filteredUsers.length <= 0 ? null : filteredUsers[0];
+    }
+
+    create(firstname, lastname, username, email, password) {
+        const foundUser = this.get(username);
+        if (foundUser == null) {
+            this.users.push({
+                firstname: firstname,
+                lastname: lastname,
+                username: username,
+                email: email,
+                password: password
+            })
+        }
+    }
+
+    /**
+     * 
+     * key = "email", "firstname", "lastname"
+     */
+    update(username, key, value) {
+        const user = this.get(username);
+        if (key === 'username') {
+            return;
+        }
+        user[key] = value;
+    }
 }
