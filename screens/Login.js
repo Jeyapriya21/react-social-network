ccf0b945d2fff48a60a5d87f3ea431b4c518ccf7
michael <nguyen.hoang.michael@gmail.com>
2020-09-29 16:55:06 +0200

    Adding styles

--- screens/Login.js
+++ screens/Login.js
@@ -33,23 +33,34 @@ export default class Login extends React.Component {
 
     render() {
         return (
+            // class="container" => style={styles.container}
             <View style={styles.container}>
-                <Text>Login</Text>
-                <TextInput value={this.state.username} onChangeText={this.handleChangeUsername} />
-                <TextInput secureTextEntry={true} value={this.state.password} onChangeText={this.handleChangePassword} />
+                <Text style={styles.textForLogin}>Bienvenue</Text>
+                <TextInput placeholder="Username ..." style={styles.inputToto} value={this.state.username} onChangeText={this.handleChangeUsername} />
+                <TextInput placeholder="Password ..." style={styles.inputToto} secureTextEntry={true} value={this.state.password} onChangeText={this.handleChangePassword} />
                 <Button title="S'identifier" onPress={this.handleLogin} />
                 <Button title="S'enregister" onPress={this.props.changeScreen('Register')} />
-                <Button title="Mot de passe oublié"  onPress={this.props.changeScreen('Forgot')} />
+                <Button title="Mot de passe oublié" onPress={this.props.changeScreen('Forgot')} />
             </View>
-          );
+        );
     }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#EEE',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: '#EEE', // background-color => backgroundColor
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    inputToto: {
+        borderWidth: 1,
+        padding: 5,
+        margin: 5,
+        borderColor: '#DDD'
+    },
+    textForLogin: {
+        fontSize: 26,
+        marginBottom: 20
+    }
 });
