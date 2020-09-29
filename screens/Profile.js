afb40dca0ca9b2419b1d7f69e6fa25f336107e34
michael <nguyen.hoang.michael@gmail.com>
2020-09-29 08:57:14 +0200

    Adding posts to profile

--- screens/Profile.js
+++ screens/Profile.js
@@ -1,14 +1,34 @@
 import React from 'react';
 import { StyleSheet, View, Text, Button } from 'react-native';
+import ServiceContext from '../ServiceContext';
 
 export default class Profile extends React.Component {
 
+    static contextType = ServiceContext;
+
     render() {
+
+        let listeDesTagsPourLesPosts = [];
+
+        const listeDePosts = this.context.postService.list();
+
+        for (const post of listeDePosts) {
+            const element = (
+                <View>
+                    <Text>{post.content}</Text>
+                    <Text>{post.likes} Like</Text>
+                    <Button title="Liker" />
+                </View>
+            );
+            listeDesTagsPourLesPosts.push(element);
+        }
+
         return (
             <View style={styles.container}>
                 <Text>Profile</Text>
                 <Button title="Voir mes amis"  onPress={this.props.changeScreen('Friends')} />
                 <Button title="Déconnexion"  onPress={this.props.changeScreen('Login')} />
+                {listeDesTagsPourLesPosts}
             </View>
           );
     }
